9466c8b485a06f9365232e4e7d357e5da53b4d97
zaakoM <zaakobusiness@gmail.com>
2022-11-06 16:58:22 +0200

    adding testing error test

---
+++ model.go
@@ -0,0 +1,15 @@
+package mainm
+
+type Trans struct {
+	from   string
+	to     string
+	amount int64
+}
+
+func (t *Trans) SendMoney(from, to string, amount int64) *Trans {
+	return &Trans{
+		amount: amount,
+		from:   from,
+		to:     to,
+	}
+}
